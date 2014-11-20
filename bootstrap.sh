3fd88d1c1807f258242a3175af9319bc71d3f364
Jared Russell <me@jaredrussell.com>
2014-11-19 18:07:28 -0800

    prettify bootstrap output slightly

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,34 +4,43 @@
 [[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME="$HOME/.config"
 [[ -z $XDG_CACHE_HOME ]]  && export XDG_CACHE_HOME="$HOME/.cache"
 
+# a simple function to indent a command's output
+indent() {
+    eval $@ |& sed 's/^/  /'
+}
+
 # check if our config directory contains our dotfiles repo
 if [[ -d $XDG_CONFIG_HOME/.git ]]; then
     # make sure it's up to date
     echo "Updating dotfiles repo in $XDG_CONFIG_HOME"
     pushd $XDG_CONFIG_HOME
-    git pull
-    git submodule update --init --recursive
+    indent git pull
+    indent git submodule update --init --recursive
     popd
 else
     # clone it
     echo "Cloning dotfiles repo to $XDG_CONFIG_HOME"
-    git clone --recursive https://github.com/fouvrai/dotfiles $XDG_CONFIG_HOME
+    indent git clone --recursive https://github.com/fouvrai/dotfiles $XDG_CONFIG_HOME
 fi
 
 # symlink all the appropriate files
+echo
 echo "Setting up symlinks in $HOME"
-symlink_files=($XDG_CONFIG_HOME/**/*.symlink)
 
-for file in $symlink_files; do
+for file in $XDG_CONFIG_HOME/**/*.symlink; do
     local home_file=$HOME/.$(basename "${file%.symlink}")
 
     if [[ ! -f $home_file ]]; then
         ln -s $file $home_file
     else
-        echo "File '$home_file' already exists."
+        indent echo "File '$home_file' already exists."
     fi
 done
 
-echo "Sourcing zsh config"
-source $XDG_CONFIG_HOME/zsh/zshenv.symlink
+# we're done with our indent function now
+unfunction indent
+
+echo
+echo "Launching ZSH"
+exec zsh
 
