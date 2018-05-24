a642492bca9f8cad2f2667687d19fe319e10b633
Jared Russell <jared@jaredru.com>
2018-05-24 15:35:27 -0700

    remove lsf alias in favor of fd tool

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,8 +9,6 @@ alias fnr='perl -i -pe'
 
 alias ls='ls -lG'
 
-alias lsf='noglob recursive-find'
-
 alias rg='rg -S'
 
 alias rmt='trash'
@@ -19,15 +17,4 @@ alias vim='nvim'
 
 psgrep() { ps up $(pgrep -f $@) 2>&-; }
 
-recursive-find() {
-    setopt local_options
-    setopt extended_glob
-    setopt glob_dots
-
-    # \       - ignores aliases
-    # (#l)    - lower case characters also match upper case
-    # ${var}  - injects a variable
-    # ${~var} - allows globbing and file expansion on the variable
-    \ls -1dG **/(#l)${~*}
-}
 
