e4af26ec11ac356af162172b65ac663cf42b1a10
Jared Russell <jared@jaredru.com>
2019-11-24 18:14:09 -0800

    Update Terminal Settings

--- terminal/bootstrap.sh
+++ terminal/bootstrap.sh
@@ -6,7 +6,7 @@ TERMINAL_PLIST=~/Library/Preferences/com.apple.Terminal.plist
 cd ${0:a:h}
 /usr/libexec/PlistBuddy \
     -c "Add ':Window Settings:Base16 Twilight' dict" \
-    -c "Merge Base16Twilight.plist ':Window Settings:Base16 Twilight'" \
+    -c "Merge 'Base16 Twilight.terminal' ':Window Settings:Base16 Twilight'" \
     -c "Set ':Default Window Settings' 'Base16 Twilight'" \
     -c "Set ':Startup Window Settings' 'Base16 Twilight'" \
     $TERMINAL_PLIST &> /dev/null || true
