80c348597f2a14c619bcddc2e6159e849a4c94a2
Jared Russell <jared@jaredru.com>
2022-09-03 08:52:37 -0700

    fix chnode pathing

--- node/chnode.zsh
+++ node/chnode.zsh
@@ -2,3 +2,10 @@
 export PATH="$PATH:$(npm -g prefix)/bin"
 source $(whence chnode)
 
+(){
+    setopt local_options nullglob
+    export NODES=(
+        $HOMEBREW_CELLAR/node/*
+        $HOMEBREW_CELLAR/node@*/*
+    )
+}
