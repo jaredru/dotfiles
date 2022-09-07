ebd24d868e6cac1d426dbdf5b76507c4c7a47375
Jared Russell <jared@jaredru.com>
2022-09-06 20:31:47 -0700

    don't fail if font types are missing

--- fonts/bootstrap.mac.sh
+++ fonts/bootstrap.mac.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 set -efu -o pipefail
 
-title "Installing fonts"
 cp -r $XDG_CONFIG_HOME/fonts/*.{o,t}tf $HOME/Library/Fonts
 
--- fonts/bootstrap.sh
+++ fonts/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 set -efu -o pipefail
 
+setopt local_options nullglob
 bootstrap-env
 
