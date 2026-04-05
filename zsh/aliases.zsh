12c9a7b5bfa6d116874c12678d9f52cfdfde00fd
Jared Russell <jared@jaredru.com>
2026-04-04 20:45:11 -0700

    add brew-deps function for reverse dependency lookup
    Single brew JSON call + bun to build the reverse dependency map,
    replacing the slow per-formula brew uses loop.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -30,3 +30,15 @@ hist() { print -z $(fc -ln 1 | fzf --tac --tiebreak=index -q "$*") }
 
 psgrep() { ps up $(pgrep -f "$*") 2>&-; }
 
+brew-deps() {
+    brew info --json=v2 --installed | bun -e "
+        const {formulae} = await Bun.file('/dev/stdin').json();
+        const u = Object.fromEntries(formulae.map(f => [f.name, []]));
+        for (const f of formulae)
+            for (const d of f.installed[0]?.runtime_dependencies ?? [])
+                u[d.full_name]?.push(f.name);
+        for (const n of Object.keys(u).sort())
+            console.log('\x1b[1;34m%s ->\x1b[0m %s', n, u[n].join(' '));
+    "
+}
+
